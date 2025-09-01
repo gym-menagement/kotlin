85ec562d25406f8e51bc52695540d73cf2759043
고범석 <kobums@naver.com>
2025-09-01 18:18:13 +0900

    init

---
+++ src/main/kotlin/com/gowoobro/gymspring/GymspringApplication.kt
@@ -0,0 +1,12 @@
+package com.gowoobro.gymspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GymspringApplication
+
+fun main(args: Array<String>) {
+	runApplication<GymspringApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/gowoobro/gymspring/GymspringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gowoobro.gymspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GymspringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
