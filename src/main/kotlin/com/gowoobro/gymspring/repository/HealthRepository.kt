2a96f6133f91585020e75957525ea0e2ad9583c9
고범석 <kobums@naver.com>
2025-11-17 22:24:56 +0900

    join 수정

--- src/main/kotlin/com/gowoobro/gymspring/repository/HealthRepository.kt
+++ src/main/kotlin/com/gowoobro/gymspring/repository/HealthRepository.kt
@@ -15,36 +15,113 @@ import java.time.LocalDateTime
 
 @Repository
 interface HealthRepository : JpaRepository<Health, Long> {
-    @EntityGraph(attributePaths = ["healthcategory", "term", "discount"])
+    @EntityGraph(attributePaths = [
+        "healthcategory",
+        "healthcategory.gym",
+        "term",
+        "term.gym",
+        "term.daytype",
+        "discount"
+    ])
     override fun findAll(pageable: Pageable): Page<Health>
 
-    @EntityGraph(attributePaths = ["healthcategory", "term", "discount"])
+    @EntityGraph(attributePaths = [
+        "healthcategory",
+        "healthcategory.gym",
+        "term",
+        "term.gym",
+        "term.daytype",
+        "discount"
+    ])
     override fun findById(id: Long): java.util.Optional<Health>
 
-    @EntityGraph(attributePaths = ["healthcategory", "term", "discount"])
+    @EntityGraph(attributePaths = [
+        "healthcategory",
+        "healthcategory.gym",
+        "term",
+        "term.gym",
+        "term.daytype",
+        "discount"
+    ])
     fun findBycategoryId(healthcategory: Long): List<Health>
 
-    @EntityGraph(attributePaths = ["healthcategory", "term", "discount"])
+    @EntityGraph(attributePaths = [
+        "healthcategory",
+        "healthcategory.gym",
+        "term",
+        "term.gym",
+        "term.daytype",
+        "discount"
+    ])
     fun findBytermId(term: Long): List<Health>
 
-    @EntityGraph(attributePaths = ["healthcategory", "term", "discount"])
+    @EntityGraph(attributePaths = [
+        "healthcategory",
+        "healthcategory.gym",
+        "term",
+        "term.gym",
+        "term.daytype",
+        "discount"
+    ])
     fun findByName(name: String): List<Health>
 
-    @EntityGraph(attributePaths = ["healthcategory", "term", "discount"])
+    @EntityGraph(attributePaths = [
+        "healthcategory",
+        "healthcategory.gym",
+        "term",
+        "term.gym",
+        "term.daytype",
+        "discount"
+    ])
     fun findByCount(count: Int): List<Health>
 
-    @EntityGraph(attributePaths = ["healthcategory", "term", "discount"])
+    @EntityGraph(attributePaths = [
+        "healthcategory",
+        "healthcategory.gym",
+        "term",
+        "term.gym",
+        "term.daytype",
+        "discount"
+    ])
     fun findByCost(cost: Int): List<Health>
 
-    @EntityGraph(attributePaths = ["healthcategory", "term", "discount"])
+    @EntityGraph(attributePaths = [
+        "healthcategory",
+        "healthcategory.gym",
+        "term",
+        "term.gym",
+        "term.daytype",
+        "discount"
+    ])
     fun findBydiscountId(discount: Long): List<Health>
 
-    @EntityGraph(attributePaths = ["healthcategory", "term", "discount"])
+    @EntityGraph(attributePaths = [
+        "healthcategory",
+        "healthcategory.gym",
+        "term",
+        "term.gym",
+        "term.daytype",
+        "discount"
+    ])
     fun findByCostdiscount(costdiscount: Int): List<Health>
 
-    @EntityGraph(attributePaths = ["healthcategory", "term", "discount"])
+    @EntityGraph(attributePaths = [
+        "healthcategory",
+        "healthcategory.gym",
+        "term",
+        "term.gym",
+        "term.daytype",
+        "discount"
+    ])
     fun findByContent(content: String): List<Health>
 
-    @EntityGraph(attributePaths = ["healthcategory", "term", "discount"])
+    @EntityGraph(attributePaths = [
+        "healthcategory",
+        "healthcategory.gym",
+        "term",
+        "term.gym",
+        "term.daytype",
+        "discount"
+    ])
     fun findByDate(date: LocalDateTime): List<Health>
 }
